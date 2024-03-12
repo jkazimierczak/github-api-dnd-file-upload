fe747cf553d51f7199a3a0bb99500b3074265d44
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-03-12 21:24:42 +0100

    feat: add drag-and drop area

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import { useCallback } from "react";
+import { DropEvent, FileRejection, useDropzone } from "react-dropzone";
+
 function App() {
+  const onDrop = useCallback(
+    (
+      acceptedFiles: File[],
+      fileRejections: FileRejection[],
+      event: DropEvent,
+    ) => {
+      console.log(acceptedFiles);
+
+      acceptedFiles.forEach((file) => {
+        console.log(file.name);
+      });
+    },
+    [],
+  );
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div
+      {...getRootProps()}
+      className="h-96 w-96 rounded bg-zinc-900 text-center text-zinc-300 shadow"
+    >
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
     </div>
   );
 }
